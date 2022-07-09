1278b8c924c42bc6180477683baba597010e8175
rivers <rivers_hyd@163.com>
2022-07-09 12:20:14 +0800

    嵌套模块

---
+++ hello2/hello3/say.go
@@ -0,0 +1,7 @@
+package hello3
+
+import "fmt"
+
+func SayHello3(name string) string {
+	return fmt.Sprintf("Hello %s!!!f", name)
+}
---
+++ hello2/say.go
@@ -0,0 +1,7 @@
+package hello2
+
+import "fmt"
+
+func SayHello2(name string) string {
+	return fmt.Sprintf("Hello %s!!", name)
+}
